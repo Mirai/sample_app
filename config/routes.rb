b02944015e3a32291318016fde877ec673a9eb94
Robert Miner <robertrminer@gmail.com>
2010-12-05 07:58:42 +0000

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'
 
   root :to => 'pages#home'
 
