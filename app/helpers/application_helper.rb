5f17d21493207d5e284f09564e3a7d5e692e7a9b
Robert Miner <robertrminer@gmail.com>
2010-12-05 11:10:33 +0000

    Added logo image helper and link spec testing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def logo
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+
   # Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
