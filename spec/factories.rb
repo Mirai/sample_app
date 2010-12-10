bcd8c72927891eee4ce45a2588d3e22c1ce7f591
Robert Miner <robertrminer@gmail.com>
2010-12-10 07:37:39 +0000

    User model with passwords, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+# By using the sybmol ':user', we get Factory Girl to simulate the User model.
+Factory.define :user do |user|
+  user.name "Michael Hartl"
+  user.email "mhartl@example.com"
+  user.password "foobar"
+  user.password_confirmation "foobar"
+end
